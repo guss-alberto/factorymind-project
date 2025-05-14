e76b20340c813435521d39670af3bfe5c457dfec
KaleidoscopRo <blagarobertother@gmail.com>
2025-05-14 17:10:09 +0200

    aggiunte le tabelle in admin

--- backend/contactsApp/admin.py
+++ backend/contactsApp/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from .models import Register, Contact
+from .models import Register, Contact, Country, Region, City, Branch, Division, Sign, Deposit, ProfilesAndSubagencies
 
 admin.site.register(Register)
 admin.site.register(Contact)
+admin.site.register(Country)
+admin.site.register(Region)
+admin.site.register(City)
+admin.site.register(Branch)
+admin.site.register(Division)
+admin.site.register(Sign)
+admin.site.register(Deposit)
+admin.site.register(ProfilesAndSubagencies)
